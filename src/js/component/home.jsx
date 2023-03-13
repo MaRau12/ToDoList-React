402c061e50fbe73cfae40b860bde6286e279e01f
MaRau12 <marthaelisabeth94@gmail.com>
2023-03-13 13:24:28 +0000

    added fetch calls, not working

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -5,21 +5,16 @@ import rigoImage from "../../img/rigo-baby.jpg";
 import { useState } from "react";
 import { List } from "./list.jsx";
 
-const url = "https://assets.breatheco.de/apis/fake/todos/user"
+const url = "https://assets.breatheco.de/apis/fake/todos/user/"
 
 //create your first component
 
 const Home = () => {
 	const [inputValue, setInputValue] = useState("")
 	const [todos, setTodos] = useState([])
+
 		useEffect(() => {
-			fetch(url, {
-				method: "PUT",
-				body: JSON.stringify(todos),
-				headers: {
-				  "Content-Type": "application/json"
-				}
-			  })
+             fetch(url)
 			  .then(resp => {
 				  console.log(resp.ok); // will be true if the response is successfull
 				  console.log(resp.status); // the status code = 200 or code = 400 etc.
@@ -33,7 +28,32 @@ const Home = () => {
 			  .catch(error => {
 				  //error handling
 				  console.log(error);
-			  })})
+			  })}, [])
+
+			  useEffect(() => {
+				fetch("https://assets.breatheco.de/apis/fake/todos/user/MaRa",
+				{
+					method: "PUT",
+					body: JSON.stringify(todos),
+					headers: {
+					  "Content-Type": "application/json"
+					}
+				}
+				)
+				 .then(resp => {
+					 console.log(resp.ok); // will be true if the response is successfull
+					 console.log(resp.status); // the status code = 200 or code = 400 etc.
+					 //console.log(resp.text()); // will try return the exact result as string
+					 return resp.json(); // (returns promise) will try to parse the result as json as return a promise that you can .then for results
+				 })
+				 .then(data => {
+					//here is were your code should start after the fetch finishes
+					console.log(data); //this will print on the console the exact object received from the server
+				})
+				 .catch(error => {
+					 //error handling
+					 console.log(error);
+				 })}, [])
 
 
     return (
