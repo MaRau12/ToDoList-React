aa8eeb8ac1c402d99f6ff90037415910a7fab394
MaRau12 <marthaelisabeth94@gmail.com>
2023-03-14 20:35:45 +0000

    better, do second useEffect:fetch:put[state]

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -5,7 +5,7 @@ import rigoImage from "../../img/rigo-baby.jpg";
 import { useState } from "react";
 import { List } from "./list.jsx";
 
-const url = "https://assets.breatheco.de/apis/fake/todos/user/"
+const url = "https://assets.breatheco.de/apis/fake/todos/user/MaRa"
 
 //create your first component
 
@@ -16,44 +16,22 @@ const Home = () => {
 		useEffect(() => {
              fetch(url)
 			  .then(resp => {
-				  console.log(resp.ok); // will be true if the response is successfull
-				  console.log(resp.status); // the status code = 200 or code = 400 etc.
-				  console.log(resp.text()); // will try return the exact result as string
+				  //console.log(resp.ok); // will be true if the response is successfull
+				  //console.log(resp.status); // the status code = 200 or code = 400 etc.
+				  //console.log(resp.text()); // will try return the exact result as string
 				  return resp.json(); // (returns promise) will try to parse the result as json as return a promise that you can .then for results
 			  })
 			  .then(data => {
+                 //setTodos(data)
 				  //here is were your code should start after the fetch finishes
-				  console.log(data); //this will print on the console the exact object received from the server
+				  //console.log(data); //this will print on the console the exact object received from the server
 			  })
 			  .catch(error => {
 				  //error handling
 				  console.log(error);
 			  })}, [])
 
-			  useEffect(() => {
-				fetch("https://assets.breatheco.de/apis/fake/todos/user/MaRa",
-				{
-					method: "PUT",
-					body: JSON.stringify(todos),
-					headers: {
-					  "Content-Type": "application/json"
-					}
-				}
-				)
-				 .then(resp => {
-					 console.log(resp.ok); // will be true if the response is successfull
-					 console.log(resp.status); // the status code = 200 or code = 400 etc.
-					 //console.log(resp.text()); // will try return the exact result as string
-					 return resp.json(); // (returns promise) will try to parse the result as json as return a promise that you can .then for results
-				 })
-				 .then(data => {
-					//here is were your code should start after the fetch finishes
-					console.log(data); //this will print on the console the exact object received from the server
-				})
-				 .catch(error => {
-					 //error handling
-					 console.log(error);
-				 })}, [])
+
 
 
     return (
